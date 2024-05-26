be71fd02c0f93b9b065d7c9afdc641e1dd4f2651
Titus Powell <tituspowell@gmail.com>
2024-05-26 13:48:26 +0100

    Config settings that produced the best yet

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ POPULATION_SIZE = 1000
 PERCENTAGE_OF_POPULATION_THAT_CAN_REPRODUCE = 0.01 # 0.01 is good - i.e. just 10 from a population of 1000. Changing this has a potent effect.
 MUTATION_RATE = 0.2 # 0.2 is about optimum
 BIAS_TOWARDS_SWITCHING_NEIGHBOURS = 0.5
-GENERATIONS = 200
+GENERATIONS = 100
